fbd68ab627ca3949738a26aec52513a477ae2509
Guilhem B. <buisanguilhem@gmail.com>
2025-12-04 22:36:46 +0100

    adds puzzle 3 boilerplate

---
+++ src/aoc2025/puzzles/3/puzzle_3.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def puzzle_3() -> int:
+    inp = (Path(__file__).parent / Path("input.txt")).read_text()
+    return 0
+
+def puzzle_3_p2() -> int:
+    inp = (Path(__file__).parent / Path("input.txt")).read_text()
+    return 0
+
+print("Puzzle 3 Part 1:", puzzle_3())
+print("Puzzle 3 Part 2:", puzzle_3_p2())
